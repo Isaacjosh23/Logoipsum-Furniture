8cca47152d17eee1b53481e354cb3839244ba508
Isaacjosh23 <isaacjoshua969@gmail.com>
2025-09-07 18:24:33 +0100

    reload fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import Logoipsum from "./components/Logoipsum";
 
 const App = () => {
+  useEffect(() => {
+    window.history.scrollRestoration = "manual";
+    window.scrollTo(0, 0);
+  }, []);
+
   return <Logoipsum />;
 };
 
